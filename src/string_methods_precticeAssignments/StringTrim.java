804442b0167d62c53297cdb5dcd9efe502888034
Jyothidevarapallii27 <jyothi.java1228@gmail.com>
2024-11-23 21:33:25 -0600

    My First Java project.

--- src/string_methods_precticeAssignments/StringTrim.java
+++ src/string_methods_precticeAssignments/StringTrim.java
@@ -3,8 +3,8 @@ package string_methods_precticeAssignments;
 public class StringTrim {
     public static void main (String []args){
         String s1 = "hello";
-        System.out.println(s1 + " how are you");
-        System.out.println(s1.trim()+"how are you");
+        System.out.println(s1 + " how are you ?");
+        System.out.println(s1.trim()+"how are you ?");
 
     }
 
